b85c79332cb6e14ebc42ff998b4c633012dd5f32
leonardoaraujo <leonardoaraujo.santos@gmail.com>
2021-08-18 23:35:56 +0100

    Working on the Multiheaded attention

--- model/sublayers/transformer.py
+++ model/sublayers/transformer.py
@@ -1,4 +1,4 @@
-'''
+"""
 Implement layers used on the transformer architecture direclty 
 
 https://nlp.seas.harvard.edu/2018/04/03/attention.html
@@ -9,7 +9,9 @@ https://discuss.pytorch.org/t/what-is-the-difference-between-register-buffer-and
 https://medium.com/the-dl/transformers-from-scratch-in-pytorch-8777e346ca51
 https://github.com/jadore801120/attention-is-all-you-need-pytorch/blob/master/transformer/SubLayers.py
 https://data-science-blog.com/blog/2021/04/07/multi-head-attention-mechanism/
-'''
+https://theaisummer.com/self-attention/
+https://www.tensorflow.org/text/tutorials/transformer
+"""
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -38,7 +40,7 @@ class WordEmbeddings(nn.Module):
 
     def forward(self, x):
         return self.lut(x) * math.sqrt(self.d_model)
-    
+
 
 class PositionwiseFeedForward(nn.Module):
     def __init__(self, d_model, d_ff, dropout=0.1):
@@ -55,7 +57,7 @@ class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout, max_len=5000):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
-        
+
         # Compute the positional encodings once in log space.
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len).unsqueeze(1)
@@ -65,96 +67,112 @@ class PositionalEncoding(nn.Module):
         pe = pe.unsqueeze(0)
         # Buffers won't participate on the optimizer phase
         self.register_buffer('pe', pe)
-        
+
     def forward(self, x):
         x = x + self.pe[:, :x.size(1)]
-        return self.dropout(x) 
-    
+        return self.dropout(x)
+
 
 def attention(query, key, value, mask=None, dropout=None):
-    # Input shape: [batch, num_heads, seq_len, depth]
+    # Input shape(key, query and values): [batch, num_heads, seq_len, depth]
     # Get the depth
     d_k = query.size(-1)
-    
+
     # This tensor multiplication is actually the place where computation and memory are most intensive
     scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
     # Apply mask (used Decoder Attention) to avoid looking into the future tokens (casual)
     if mask is not None:
         scores = scores.masked_fill(mask == 0, -1e9)
-    
+
     # The attention tensor is used more for debugging
-    p_attn = F.softmax(scores, dim = -1)
+    attn_weights = F.softmax(scores, dim=-1)
     if dropout is not None:
-        p_attn = dropout(p_attn)
-        
-    # Output shape: [batch, num_heads, seq_len, depth]
-    return torch.matmul(p_attn, value), p_attn
-    
+        attn_weights = dropout(attn_weights)
+
+    # Scaled attention: [batch, num_heads, seq_len, depth]
+    # Attention weights: [batch_size, num_heads, seq_len_q, seq_len_k]
+    return torch.matmul(attn_weights, value), attn_weights
+
 
 class MultiHeadedAttention(nn.Module):
     def __init__(self, num_heads, d_model, dropout=0.1):
         super().__init__()
-        assert d_model % h == 0
-        # We assume d_v always equals d_k
-        self.d_k = d_model // num_heads
-        self.h = num_heads
-        self.linears = clones(nn.Linear(d_model, d_model), 4)
-        self.attn = None
+        assert d_model % num_heads == 0
+        self.depth = d_model // num_heads
+        self.num_heads = num_heads
+        # Instantiate the linear layers for Key, Query, Value
+        self.linear_q = nn.Linear(d_model, d_model)
+        self.linear_k = nn.Linear(d_model, d_model)
+        self.linear_v = nn.Linear(d_model, d_model)
+        self.linear_concat = nn.Linear(d_model, d_model)
+        self.attn_weights = None
         self.dropout = nn.Dropout(p=dropout)
-        
+
     def forward(self, query, key, value, mask=None):
         if mask is not None:
             # Same mask applied to all h heads.
             mask = mask.unsqueeze(1)
-        nbatches = query.size(0)
-        
-        # 1) Do all the linear projections in batch from d_model => h x d_k 
-        query, key, value = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2) 
-                             for l, x in zip(self.linears, (query, key, value))]
-        
-        # Run all attentions in parallel
-        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
-        
-        # 3) "Concat" using a view and apply a final linear. 
-        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
-        return self.linears[-1](x)
-    
-    
+        num_batches = query.size(0)
+
+        # Apply linear layers to Key,Query,Value their input shape: [batch, seq_len, d_model]
+        q = self.linear_q(query)
+        k = self.linear_k(key)
+        v = self.linear_v(value)
+
+        # Split d_model into [batch_size, num_heads, seq_len, depth]
+        q = q.view(num_batches, -1, self.num_heads, self.depth).transpose(1, 2)
+        k = k.view(num_batches, -1, self.num_heads, self.depth).transpose(1, 2)
+        v = v.view(num_batches, -1, self.num_heads, self.depth).transpose(1, 2)
+
+        # Compute the scaled attention: [batch_size, num_heads, seq_len, depth] and
+        # Attention weights: [batch_size, num_heads, seq_len_q, seq_len_k]
+        scaled_attn, self.attn_weights = attention(query, key, value, mask=mask, dropout=self.dropout)
+
+        # Concat the num_heads and depth (back to d_model) into shape [batch_size, seq_len, d_model]
+        concat_scaled_attn = scaled_attn.transpose(1, 2).contiguous().view(num_batches, -1, self.num_heads * self.depth)
+        output = self.linear_concat(concat_scaled_attn)
+        return output
+
 
 class Encoder(nn.Module):
-    "Core encoder is a stack of N layers"
+    """
+    Core encoder is a stack of N layers"
+    """
+
     def __init__(self, layer, N):
         super().__init__()
         self.layers = clones(layer, N)
         self.norm = nn.LayerNorm(layer.size)
-        
+
     def forward(self, x, mask):
         "Pass the input (and mask) through each layer in turn."
         for layer in self.layers:
             x = layer(x, mask)
         return self.norm(x)
-    
 
 
 class Decoder(nn.Module):
-    "Generic N layer decoder with masking."
+    """
+    Generic N layer decoder with masking.
+    """
+
     def __init__(self, layer, N):
         super().__init__()
         self.layers = clones(layer, N)
         self.norm = nn.LayerNorm(layer.size)
-        
+
     def forward(self, x, memory, src_mask, tgt_mask):
         for layer in self.layers:
             x = layer(x, memory, src_mask, tgt_mask)
         return self.norm(x)
-    
-    
-    
+
+
 class SublayerConnection(nn.Module):
     """
     A residual connection followed by a layer norm.
     Note for code simplicity the norm is first as opposed to last.
     """
+
     def __init__(self, size, dropout):
         super().__init__()
         self.norm = nn.LayerNorm(size)
@@ -163,11 +181,13 @@ class SublayerConnection(nn.Module):
     def forward(self, x, sublayer):
         "Apply residual connection to any sublayer with the same size."
         return x + self.dropout(sublayer(self.norm(x)))
-    
-    
-    
+
+
 class EncoderLayer(nn.Module):
-    "Encoder is made up of self-attn and feed forward (defined below)"
+    """
+    Encoder is made up of self-attn and feed forward (defined below)
+    """
+
     def __init__(self, size, self_attn, feed_forward, dropout):
         super().__init__()
         self.self_attn = self_attn
@@ -176,14 +196,15 @@ class EncoderLayer(nn.Module):
         self.size = size
 
     def forward(self, x, mask):
-        "Follow Figure 1 (left) for connections."
         x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
         return self.sublayer[1](x, self.feed_forward)
-    
-    
+
 
 class DecoderLayer(nn.Module):
-    "Decoder is made of self-attn, src-attn, and feed forward (defined below)"
+    """
+    Decoder is made of self-attn, src-attn, and feed forward (defined below)
+    """
+
     def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
         super().__init__()
         self.size = size
@@ -191,9 +212,8 @@ class DecoderLayer(nn.Module):
         self.src_attn = src_attn
         self.feed_forward = feed_forward
         self.sublayer = clones(SublayerConnection(size, dropout), 3)
- 
+
     def forward(self, x, memory, src_mask, tgt_mask):
-        "Follow Figure 1 (right) for connections."
         m = memory
         x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
         x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
